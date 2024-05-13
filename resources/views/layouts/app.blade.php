cfbc5163f9a2ae161f0767e0b22e0d6e05572be3
ummadam <alimzhanova_dinara90@mail.ru>
2024-05-13 17:06:29 +0500

    last changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,9 +24,25 @@
 <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/css/blog.css') }}" rel="stylesheet">
 
-<meta name="yandex-verification" content="d87d4f3ba4bbe051" />
+<!-- <meta name="yandex-verification" content="d87d4f3ba4bbe051" /> -->
 
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(97245871, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/97245871" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 </head>
 <body class="antialiased">
 @include('partials.header')
