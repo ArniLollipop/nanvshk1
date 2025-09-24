4498ae0854a2b8fa747058682afe3ad8ba7f6f5d
Arnur Kushenov <bocunahero@gmail.com>
2025-09-24 21:11:15 +0500

    harcoded date for 3 element

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('content')  
+@section('content')
 
 <div class="boxed_wrapper">
 <!-- Page Title -->
@@ -29,38 +29,65 @@
     <div class="auto-container">
         <div class="row clearfix">
         @forelse ($news as $info)
-            <div class="col-lg-4 col-md-6 col-sm-12 news-block">
-                <div class="news-block-one wow fadeInUp animated animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                    <div class="inner-box p_relative d_block mb_70">
-                        <div class="image-box p_relative d_block">
-                       
-                            <figure class="image p_relative d_block"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}" class="img--change" aria-label="Переход на {{ $info->title }}"><img src="{{ asset('storage/'.$info->image) }}" alt=""></a></figure>
-                            <div class="post-date-two p_absolute l_30 t_30 w_60 centred pt_10 pb_10 b_shadow_6"><h4 class="fs_20 font_family_oxygen fw_bold lh_20 text--resize">{{ $info->created_at->format('d') }}<span class="d_block fs_14 text--resize">{{ $info->created_at->format('M') }}</span></h4></div>
-                        </div>
-                        <div class="lower-content p_relative d_block pt_25">
-                                                @if(app()->getLocale() == 'ru')
-                                                <h4 class="d_block fs_20 lh_30 mb_6 text--resize"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}"  aria-label="Переход на {{ $info->title }}">{{ $info->title }}</a></h4>
-                                            
-                                                 @elseif(app()->getLocale() == 'kz')
-                                                 <h4 class="d_block fs_20 lh_30 mb_6 text--resize"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}"  aria-label="Переход на {{ $info->title_kz }}">{{ $info->title_kz }}</a></h4>
+        <div class="col-lg-4 col-md-6 col-sm-12 news-block">
+            <div class="news-block-one wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                <div class="inner-box p_relative d_block mb_70">
+                    <div class="image-box p_relative d_block">
 
-                                                @else
-                                                <h4 class="d_block fs_20 lh_30 mb_6 text--resize"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}"  aria-label="Переход на {{ $info->title_en }}">{{ $info->title_en }}</a></h4>
-                                            
-                                                @endif
-                          
+                        <figure class="image p_relative d_block">
+                            <a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}"
+                            class="img--change"
+                            aria-label="Переход на {{ $info->title }}">
+                            <img src="{{ asset('storage/'.$info->image) }}" alt="">
+                            </a>
+                        </figure>
+
+                        <div class="post-date-two p_absolute l_30 t_30 w_60 centred pt_10 pb_10 b_shadow_6">
+                            <h4 class="fs_20 font_family_oxygen fw_bold lh_20 text--resize">
+                                @if ($loop->index === 2) {{-- 3-й элемент (индексация с нуля) --}}
+                                    01
+                                    <span class="d_block fs_14 text--resize">{{ $info->created_at->format('M') }}</span>
+                                @else
+                                    {{ $info->created_at->format('d') }}
+                                    <span class="d_block fs_14 text--resize">{{ $info->created_at->format('M') }}</span>
+                                @endif
+                            </h4>
                         </div>
+
                     </div>
+
+                <div class="lower-content p_relative d_block pt_25">
+                    @if(app()->getLocale() == 'ru')
+                        <h4 class="d_block fs_20 lh_30 mb_6 text--resize">
+                            <a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}" aria-label="Переход на {{ $info->title }}">
+                                {{ $info->title }}
+                            </a>
+                        </h4>
+                    @elseif(app()->getLocale() == 'kz')
+                        <h4 class="d_block fs_20 lh_30 mb_6 text--resize">
+                            <a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}" aria-label="Переход на {{ $info->title_kz }}">
+                                {{ $info->title_kz }}
+                            </a>
+                        </h4>
+                    @else
+                        <h4 class="d_block fs_20 lh_30 mb_6 text--resize">
+                            <a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}" aria-label="Переход на {{ $info->title_en }}">
+                                {{ $info->title_en }}
+                            </a>
+                        </h4>
+                    @endif
                 </div>
             </div>
-        @empty
-		<div style="text-align: left" class="text--resize">Пока нет новостей</div>
-		@endforelse 
         </div>
-        @if(!is_array($news))
-		{{ $news->appends(request()->input())->links('pagination') }}
-		@endif
-         
+    </div>
+    @empty
+    <div style="text-align: left" class="text--resize">Пока нет новостей</div>
+    @endforelse
+    @if(!is_array($news))
+    {{ $news->appends(request()->input())->links('pagination') }}
+    @endif
+
+
     </div>
 </section>
 <!-- blog-grid-one end -->
--- resources/views/search-results.blade.php
+++ resources/views/search-results.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-@section('content')  
+@section('content')
 
 <div class="boxed_wrapper">
 <!-- Page Title -->
@@ -33,34 +33,34 @@
                 <div class="news-block-one wow fadeInUp animated animated" data-wow-delay="00ms" data-wow-duration="1500ms">
                     <div class="inner-box p_relative d_block mb_70">
                         <div class="image-box p_relative d_block">
-                       
+
                             <figure class="image p_relative d_block"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}"><img src="{{ asset('storage/'.$info->image) }}" alt=""></a></figure>
                             <div class="post-date-two p_absolute l_30 t_30 w_60 centred pt_10 pb_10 b_shadow_6"><h4 class="fs_20 font_family_oxygen fw_bold lh_20">{{ $info->created_at->format('d') }}<span class="d_block fs_14">{{ $info->created_at->format('M') }}</span></h4></div>
                         </div>
                         <div class="lower-content p_relative d_block pt_25">
-                                                @if(app()->getLocale() == 'ru')
-                                                <h4 class="d_block fs_20 lh_30 mb_6"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}">{{ $info->title }}</a></h4>
-                                            
-                                                 @elseif(app()->getLocale() == 'kz')
-                                                 <h4 class="d_block fs_20 lh_30 mb_6"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}">{{ $info->title_kz }}</a></h4>
+                            @if(app()->getLocale() == 'ru')
+                            <h4 class="d_block fs_20 lh_30 mb_6"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}">{{ $info->title }}</a></h4>
 
-                                                @else
-                                                <h4 class="d_block fs_20 lh_30 mb_6"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}">{{ $info->title_en }}</a></h4>
-                                            
-                                                @endif
-                          
+                                @elseif(app()->getLocale() == 'kz')
+                                <h4 class="d_block fs_20 lh_30 mb_6"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}">{{ $info->title_kz }}</a></h4>
+
+                            @else
+                            <h4 class="d_block fs_20 lh_30 mb_6"><a href="{{ route('news-details', ['info' => $info->slug, app()->getLocale()] ) }}">{{ $info->title_en }}</a></h4>
+
+                            @endif
+
                         </div>
                     </div>
                 </div>
             </div>
         @empty
 		<div style="text-align: left">По вашему запросу ничего не найдено</div>
-		@endforelse 
+		@endforelse
         </div>
         @if(!is_array($news))
 		{{ $news->appends(request()->input())->links('pagination') }}
 		@endif
-         
+
     </div>
 </section>
 <!-- blog-grid-one end -->
